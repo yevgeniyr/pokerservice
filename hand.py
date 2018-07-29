d80c0003b77c8b327aee35b6ee9366b0f9282836
yevgeniy <yevgeniy@yevgeniys-MacBook-Pro.local>
2018-07-28 21:36:52 -0400

    cleaning up

--- hand.py
+++
@@ -1,8 +0,0 @@
-import card
-
-class Hand():
-    def __init__(self,hand_str,deck_str):
-        self.cards = handstr.split(' ')
-        for acard in self.cards:
-            assert acard in card.
-
